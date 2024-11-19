b91790bbec3a3619fad7d5544c92adab6de21437
paulccccc <paul_ccc@outlook.com>
2024-11-19 15:07:31 +0800

    feat: add asBTC TVL

--- projects/astherus/index.js
+++ projects/astherus/index.js
@@ -5,9 +5,20 @@ const config = {
   bsc: '0x128463A60784c4D3f46c23Af3f65Ed859Ba87974',
   ethereum: '0x604DD02d620633Ae427888d41bfd15e38483736E'
 }
+const asBTCContract = '0x184b72289c0992BDf96751354680985a7C4825d6'
 
 module.exports = {
   start: 1706716800, // 02/01/2024 @ 00:00:00pm (UTC)
+}
+
+const getAsBTCTVL = async () => {
+  const { data: asTokensPrice } = await getConfig(`astherus/token-price`, `https://www.astherus.finance/bapi/futures/v1/public/future/earn/getAssTokenPrice`)
+  
+  const assetIndexs = await getConfig(`astherus/asset-index`, `https://fapi.apollox.finance/fapi/v1/assetIndex`)
+  const { asTokenPrice, asTokenTotalSupply} = asTokensPrice?.find(token => token.assTokenName === 'asBTC') ?? {}
+  const btcPrice = assetIndexs?.find(asset => asset.symbol === 'BTCUSD')?.index
+  
+  return asTokenPrice * asTokenTotalSupply * btcPrice
 }
 
 Object.keys(config).forEach(chain => {
@@ -16,8 +27,12 @@ Object.keys(config).forEach(chain => {
     tvl: async (api) => {
       const { data } = await getConfig(`astherus/${api.chain}`, `https://astherus.finance/bapi/futures/v1/public/future/web3/ae-deposit-asset?chainId=${api.chainId}`)
       const tokens = data.map(i => i.contractAddress)
+      if (chain === 'bsc') {
+        const asBTCTVL = [await getAsBTCTVL() ]
+        api.add(asBTCContract, asBTCTVL)
+      }
       return api.sumTokens({ owner: vault, tokens })
-    }
+    },
   }
 })
 
